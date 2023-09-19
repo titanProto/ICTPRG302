2cba166d677a91b4a26e7674e8f3d08c23a0471d
Titan <TitanProto@pm.me>
2023-09-19 12:12:56 +0930

    Update test.py
    just more examples and stuff

--- test.py
+++ test.py
@@ -13,22 +13,36 @@
 #                                 |___/
 ## This is a general code testing file, it will be used to test anything and generate code for examples in assignments for this subject
 
-#ASSESSMENT 1
-a = 15
-b = 7
+#ASSESSMENT 1 examples
+# no longer required //
+# a = 15
+# b = 7
 
-def coolFunction():
-    global c
-    c = "abc"
-    x = True
-    print(a)
-    print(b)
-    print(c)
-    print(x)
+# def coolFunction():
+#     global c
+#     c = "abc"
+#     x = True
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(x)
 
-coolFunction()
+# coolFunction()
 
-print(a)
-print(b)
-print(c)
-# print(x) #won't print, not globalled
+# print(a)
+# print(b)
+# print(c)
+# # print(x) #won't print, not globalled
+
+# # lists
+# myList = [1, "b", "abc"]
+# print(myList[2])
+
+# # dictionaries
+# myDictionary = {
+#     "brand": "Holden",
+#     "model": "Commodore",
+#     "colour": "White",
+#     "year": 2003
+# }
+# print(myDictionary["brand"])
